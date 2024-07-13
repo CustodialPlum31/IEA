144f2df8b6a29b1a5164e6a2b1b5ff2ececba673
Custodial <mcejad1600@alumno.ipn.mx>
2024-07-13 01:31:41 -0600

    Primer intento

--- database/db.sql
+++ database/db.sql
@@ -3,8 +3,8 @@ CREATE DATABASE ieamysql;
 
 USE ieamysql;
 
-CREATE TABLE investigadores(
-    id_i  INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE investigador(
+    id  INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     correo VARCHAR(100) NOT NULL,
     telefono VARCHAR(10) NOT NULL,
@@ -25,20 +25,20 @@ CREATE TABLE admin(
     contrasena varchar(20) NOT NULL
 );
 
-CREATE TABLE equipos(
+CREATE TABLE equipo(
     id_e  INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     descripcion TEXT NOT NULL
 );
 
-CREATE TABLE componentes(
+CREATE TABLE componente(
     id_c  INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     valor VARCHAR(100) NOT NULL,
     cantidad int(10) NOT NULL
 );
 
-CREATE TABLE prestamos(
+CREATE TABLE prestamo(
     id_p  INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     investigador_id INT(6) UNSIGNED,
 	equipos_id INT(6) UNSIGNED,
