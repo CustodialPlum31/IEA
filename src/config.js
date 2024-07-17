8b93e62752d95b3d8cd82c5f6b3d13e0732a5074
Custodial <mcejad1600@alumno.ipn.mx>
2024-07-17 17:58:00 -0600

    config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // config.js
 const PORT = process.env.PORT || 3000;
-const DB_HOST =  'localhost';
-const DB_USER =  'root';
-const DB_PASSWORD = 'Up1_M4N12#';
-const DB_NAME =  'ieamysql';
-const DB_PORT =  3306;
+const DB_HOST =  process.env.PORT ||'localhost';
+const DB_USER = process.env.PORT || 'root';
+const DB_PASSWORD = process.env.PORT || 'Up1_M4N12#';
+const DB_NAME = process.env.PORT || 'ieamysql';
+const DB_PORT = process.env.PORT || 3306;
 
 module.exports = {
     PORT,
